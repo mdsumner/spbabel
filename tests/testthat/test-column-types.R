6ff0efe8721939ba76b214d74b4efa4623f4d764
Michael Sumner <Michael.Sumner@aad.gov.au>
2016-07-28 13:54:08 +1000

    added test for branch/object as character and factor

---
+++ tests/testthat/test-column-types.R
@@ -0,0 +1,23 @@
+context("column-types")
+
+data(holey)
+library(dplyr)
+data(holey)
+sph <- sp(holey)  ## ok
+
+library(dplyr)
+h1 <- holey %>% mutate(object_ = as.character(object_))
+h2 <- holey %>% mutate(branch_ = as.character(branch_ ))
+h3 <- holey %>% mutate(branch_ = factor(branch_ ))
+h4 <- holey %>% mutate(object_= factor(object_ ))
+
+test_that("branch and object can be character", {
+  expect_that(sp(h1), is_a(class(sph)))
+  expect_that(sp(h2), is_a(class(sph)))
+})
+
+test_that("branch and object can be factor", {
+  expect_that(sp(h3), is_a(class(sph)))
+  expect_that(sp(h4), is_a(class(sph)))
+})
+
