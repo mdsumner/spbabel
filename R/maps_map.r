2d482ea2f6d7d191da0fca613834d84063cf79ef
Michael Sumner <mdsumner@gmail.com>
2016-08-14 12:37:04 +1000

    stub of sp.map method for the polys version

---
+++ R/maps_map.r
@@ -0,0 +1,76 @@
+## logic to make this work is
+## check length of seqs vs. length of names
+## if length(unique(names)) is not equal to length(seqs) 
+##    return LINES, with no attributes
+## otherwise
+##    returns SPoly with names as attribute on objects
+## ignore part to object union, as that should happen on the arcs
+sp.map <- function(x) {
+  bad <- is.na(x$x)
+  ## TODO check badx and bady are the same
+  partsID <- cumsum(bad) + 1L
+  
+  ## bad drops all the polygon list bounds
+  coords <- tibble::as_tibble(list(x_ = x$x, y_ = x$y, branch_ = partsID))[!bad, ]
+  coords$object_ <- coords$branch_ 
+  coords$island_ <- TRUE  ## hmm needs work
+  coords$order_ <- seq(nrow(coords))
+  
+  length(unique(coords$branch_))
+  partname <- x$names
+  partname[duplicated(partname)] <- paste0(partname[duplicated(partname)], ":1")
+  
+  obj <- unlist(lapply(strsplit(partname, ":"), "[", 1L))
+  partiden <- unlist(lapply(strsplit(x$names, ":"), tail, 1L))
+  
+  coords$object_ <- obj[coords$branch_]
+  coords$branch_ <- partiden[coords$branch_]
+  
+  spbabel::sp(coords)         
+}
+
+## Possibly, can resurrect polygons from the arcs, but there's still no attributes
+# library(maps)
+# library(tibble)
+# 
+# 
+# fcounty <- map("county", fill = TRUE, plot = FALSE)
+# lcounty <- map("county", fill = FALSE, plot = FALSE)
+# 
+# str(fcounty)
+# str(lcounty)
+# 
+# miss <- is.na(lcounty$x)
+# seqs <- cumsum(miss)
+# v <- tibble(x = lcounty$x, y = lcounty$y, group = seqs)[!miss, ]
+# 
+# ## non-unique instances of vertices
+# ## with ide of original arc sequence
+# vi <- v %>% mutate(.vx0 = as.integer(factor(paste(x, y, sep = "_")))) 
+# 
+# ## unique vertices
+# vu <- vi %>% dplyr::select(x, y, .vx0) %>% distinct()
+# vi <- vi %>% dplyr::select(.vx0, group) 
+# 
+# ## trace each group
+# topbot <- vi %>% group_by(group) %>% slice(c(1, n()))
+# 
+# topbot %>% filter(.vx0 == 29897)
+# non_uniques <- vi[duplicated(vi$.vx0), ]
+# polys <- list()
+# for (jj in unique(non_uniques$.vx0)) {
+#   groups <- subset(non_uniques, .vx0 == jj)$group
+#   xx <- vi %>% dplyr::filter(group %in% groups) %>% inner_join(vu) 
+#   plot(xx$x, xx$y)
+#   lapply(split(xx, xx$group), function(x) lines(x$x, x$y, col = "red"))
+#   scan("", 1)
+# }
+# 
+# 
+# library(ggplot2)
+# ggplot(v) + aes(x = x, y = y, group = group, col = group ) + geom_line() + 
+#   scale_color_continuous(guide = FALSE)
+# 
+# distinct(v)
+
+
