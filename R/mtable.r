7825d2d1981f60790e6e84d29ab4f7e4bfe291f7
Michael Sumner <mdsumner@gmail.com>
2016-08-24 23:47:10 +1000

    Update mtable.r

--- R/mtable.r
+++ R/mtable.r
@@ -23,6 +23,15 @@
 # }
 
 
+semi_join_be_quiet_if_there_is_only_1 <- function(x, y, by = NULL, copy = FALSE, ...) {
+  comm <- intersect(names(x), names(y))
+  if (length(comm) == 1L) {
+    by <- comm
+  }
+  semi_join(x, y, by = by, copy = copy, ...)
+}
+
+
 #' Cascading subset on object for \code{map_table}. 
 #'
 #' @param x 
@@ -34,7 +43,7 @@ semi_cascade <- function(x, ..., tables = c("o", "b", "bXv", "v")) {
   x[[1]] <- last <- first 
   tables <- tables[-1]
   for (itab in tables) {
-    x[[itab]] <- last <- dplyr::semi_join(x[[itab]], last)
+    x[[itab]] <- last <- semi_join_be_quiet_if_there_is_only_1(x[[itab]], last)
   }
   x
 }
