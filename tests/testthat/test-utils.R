706b7042021f6f9052b44c4e6a25a16f376ee3eb
Michael Sumner <mdsumner@gmail.com>
2016-09-21 16:43:12 +1000

    update tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -11,9 +11,9 @@ test_that("utils tests work", {
   expect_that(spbabel:::has_data(sph), is_true())
   expect_that(spbabel:::has_names(sph), is_true())
   expect_that(spbabel:::has_names(x), is_false())
-  expect_output(print(sph))
-  expect_output(print(as(sph, "SpatialLinesDataFrame")))
-  expect_output(print(as(as(sph, "SpatialLinesDataFrame"), "SpatialPointsDataFrame")))
-  expect_output(print(as(as(sph, "SpatialLinesDataFrame"), "SpatialMultiPointsDataFrame")))
+  expect_output(show(sph))
+  expect_output(show(as(sph, "SpatialLinesDataFrame")))
+  expect_output(show(as(as(sph, "SpatialLinesDataFrame"), "SpatialPointsDataFrame")))
+  expect_output(show(as(as(sph, "SpatialLinesDataFrame"), "SpatialMultiPointsDataFrame")))
   
 })
