e3bf4b91aa06f77b78976dfc109cc46e3319c766
Michael Sumner <mdsumner@gmail.com>
2016-09-21 16:38:07 +1000

    remove unused sepNA

--- R/raster_geom.r
+++ R/raster_geom.r
@@ -3,18 +3,18 @@ rasterpoly <- function (x, sepNA = FALSE, ...)
     nobs <- length(x@polygons)
     objlist <- list()
     cnt <- 0
-    if (sepNA) {
-      sep <- rep(NA, 5)
-      for (i in 1:nobs) {
-        nsubobs <- length(x@polygons[[i]]@Polygons)
-        ps <- lapply(1:nsubobs, function(j) rbind(cbind(j, 
-                                                        j + cnt, x@polygons[[i]]@Polygons[[j]]@hole, 
-                                                        x@polygons[[i]]@Polygons[[j]]@coords), sep))
-        objlist[[i]] <- cbind(i, do.call(rbind, ps))
-        cnt <- cnt + nsubobs
-      }
-    }
-    else {
+    # if (sepNA) {
+    #   sep <- rep(NA, 5)
+    #   for (i in 1:nobs) {
+    #     nsubobs <- length(x@polygons[[i]]@Polygons)
+    #     ps <- lapply(1:nsubobs, function(j) rbind(cbind(j, 
+    #                                                     j + cnt, x@polygons[[i]]@Polygons[[j]]@hole, 
+    #                                                     x@polygons[[i]]@Polygons[[j]]@coords), sep))
+    #     objlist[[i]] <- cbind(i, do.call(rbind, ps))
+    #     cnt <- cnt + nsubobs
+    #   }
+    # }
+    # else {
       for (i in 1:nobs) {
         nsubobs <- length(x@polygons[[i]]@Polygons)
         ps <- lapply(1:nsubobs, function(j) cbind(j, 
@@ -23,14 +23,14 @@ rasterpoly <- function (x, sepNA = FALSE, ...)
         objlist[[i]] <- cbind(i, do.call(rbind, ps))
         cnt <- cnt + nsubobs
       }
-    }
+    #}
     obs <- do.call(rbind, objlist)
     colnames(obs) <- c("object", "part", "cump", "hole", 
                        "x", "y")
     rownames(obs) <- NULL
-    if (sepNA) {
-      obs[is.na(obs[, 2]), ] <- NA
-    }
+    #if (sepNA) {
+    #  obs[is.na(obs[, 2]), ] <- NA
+    #}
     obs
   }
 
@@ -38,17 +38,17 @@ rasterline <-  function (x, sepNA = FALSE, ...)  {
   nobs <- length(x@lines)
   objlist <- list()
   cnt <- 0
-  if (sepNA) {
-    sep <- rep(NA, 4)
-    for (i in 1:nobs) {
-      nsubobj <- length(x@lines[[i]]@Lines)
-      ps <- lapply(1:nsubobj, function(j) rbind(cbind(j, 
-                                                      j + cnt, x@lines[[i]]@Lines[[j]]@coords), sep))
-      objlist[[i]] <- cbind(i, do.call(rbind, ps))
-      cnt <- cnt + nsubobj
-    }
-  }
-  else {
+  # if (sepNA) {
+  #   sep <- rep(NA, 4)
+  #   for (i in 1:nobs) {
+  #     nsubobj <- length(x@lines[[i]]@Lines)
+  #     ps <- lapply(1:nsubobj, function(j) rbind(cbind(j, 
+  #                                                     j + cnt, x@lines[[i]]@Lines[[j]]@coords), sep))
+  #     objlist[[i]] <- cbind(i, do.call(rbind, ps))
+  #     cnt <- cnt + nsubobj
+  #   }
+  # }
+  #else {
     for (i in 1:nobs) {
       nsubobj <- length(x@lines[[i]]@Lines)
       ps <- lapply(1:nsubobj, function(j) cbind(j, 
@@ -56,12 +56,12 @@ rasterline <-  function (x, sepNA = FALSE, ...)  {
       objlist[[i]] <- cbind(i, do.call(rbind, ps))
       cnt <- cnt + nsubobj
     }
-  }
+  #}
   obs <- do.call(rbind, objlist)
   colnames(obs) <- c("object", "part", "cump", "x", "y")
   rownames(obs) <- NULL
-  if (sepNA) {
-    obs[is.na(obs[, 2]), ] <- NA
-  }
+  #if (sepNA) {
+  #  obs[is.na(obs[, 2]), ] <- NA
+  #}
   obs
 }
