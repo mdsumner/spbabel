90023e0ab1e997ec03764afbcd14831c283cde9d
Michael Sumner <mdsumner@gmail.com>
2017-07-02 15:16:17 +1000

    xy_ imports

--- R/xy_.R
+++ R/xy_.R
@@ -3,18 +3,21 @@
 #' A generic version of sp's 'coordinates' to get a centroid.
 #' 
 #' The default method handles any sp or sf object
-#' @importFrom dplyr select
+#' @importFrom dplyr select group_by summarize %>% 
+#' @importFrom tibble as_tibble
+#' @importFrom stats setNames
+#' @importFrom sp coordinates
 #' @name xy_
 #' @export
 xy_ <- function(x) UseMethod("xy_")
 #' @name xy_
 #' @export
 xy_.default <- function(x) {
-  dplyr::select(spbabel::sptable(x) %>% 
-                  group_by(object_) %>% 
+  dplyr::select(sptable(x) %>% 
+                  dplyr::group_by(object_) %>% 
                   ## this is where ketchup should be used
                   ## to handle different kinds of centroids
-                  summarize(x_ = mean(x_), y_ = mean(y_)), x_, y_)
+                  dplyr::summarize(x_ = mean(x_), y_ = mean(y_)), x_, y_)
 }
 #' @name xy_
 #' @export
